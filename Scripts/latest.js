cb9be3d8180f79e5a19b20e0afd1be8176fa2299
SaraHussein4 <saraomran12002@gmail.com>
2025-05-01 18:54:50 +0300

    update style

--- Scripts/latest.js
+++ Scripts/latest.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const authorName = author ? author.name : "Unknown author";
 
             const BookHTML = `
-               <div class="col-md-3 col-sm-6 col-6">
+               <div class="col-md-3 col-sm-6 col-6" style="height:500px">
   <div class="product-item">
     <div class="product-style">
       <img src="${image}" alt="Books" class="product-image" style="height:360px;">
